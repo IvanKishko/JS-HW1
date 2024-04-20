210367dd99651822e8c8b722ed3cf568e5722395
Ivan Kishko1~ <oknavi779@gmail.com>
2024-04-20 10:22:32 +0200

    HW-1

---
+++ index.js
@@ -0,0 +1,28 @@
+// TASK 1
+let age = 14;
+console.log(age);
+// TASK 2
+const name = "Ivan";
+console.log(name);
+// Task 3
+let isStudent = true;
+console.log(isStudent);
+// TASK 4
+let myString =
+  "“Tell me why are we so blind to see, that the ones we hurt are you and me?” ©Coolio";
+console.log(myString);
+// TASK 5
+let myNumber = 8;
+let res = myNumber + 10;
+console.log(res);
+// TASK 6
+const myNull = null;
+console.log(typeof myNull);
+// TASK 7
+let userName = prompt("Введіть ваше ім'я");
+alert("Вітаємо, " + userName);
+// TASK 8
+const submitAction = confirm("Підтвердіть вашу дію");
+
+// TASK 9
+const dangerAction = alert("Ця дія небезпечна. Ви впевнені?");
